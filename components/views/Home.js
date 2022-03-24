a1284873a83d314bec9444468543384f5b2e065d
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-03-23 21:49:46 -0500

    images

--- components/views/Home.js
+++ components/views/Home.js
@@ -31,7 +31,7 @@ export default st => html`
   <div class="column">
   <div class="column-header">
       <center><h3>Relax</h3></center>
-    <img src="https://cdn-icons.flaticon.com/png/512/3671/premium/3671163.png?token=exp=1648088991~hmac=0f8379530afbff01d01f52e20e664af5" alt="Relaxing Getting A Hair Cut" style="width:90%">
+    <img src="https://cdn-icons-png.flaticon.com/512/2798/2798223.png" alt="Relaxing Getting A Hair Cut" style="width:90%">
     <div class="colum-content"> 
        <p>Sit back and relax! Professional service in your home, on your time.</p>
       </div>
