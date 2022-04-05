14115837a0ae5f51a0c4293e79df8b314b95500a
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-04-04 23:02:03 -0500

    Navigation Changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,7 @@
 import html from "html-literal";
 
 export default () => html`
-  <footer>&copy; 2022 <a href="/">In A Snip</a> | <a href="./About">About</a> | <a href="./Contact">Contact</a></footer>
+<div id="footer">
+  <footer>&copy; 2022 All Rights Reserved | <a href="/">In A Snip</a> | <a href="./About">About</a> | <a href="./Contact">Contact</a></footer>
+  </div>
 `;
--- dist/index.3d214d75.js
+++ dist/index.3d214d75.js
@@ -1348,7 +1348,9 @@ parcelHelpers.defineInteropFlag(exports);
 var _htmlLiteral = require("html-literal");
 var _htmlLiteralDefault = parcelHelpers.interopDefault(_htmlLiteral);
 exports.default = ()=>_htmlLiteralDefault.default`
-  <footer>&copy; 2022 <a href="/">In A Snip</a> | <a href="./About">About</a> | <a href="./Contact">Contact</a></footer>
+<div id="footer">
+  <footer>&copy; 2022 All Rights Reserved | <a href="/">In A Snip</a> | <a href="./About">About</a> | <a href="./Contact">Contact</a></footer>
+  </div>
 `
 ;
 
